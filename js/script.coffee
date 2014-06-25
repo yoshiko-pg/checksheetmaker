8ac891aaee24e2be84a32e084f823f50ac3ce180
maasa <maasa.giss2@gmail.com>
2014-06-25 12:58:30 +0900

    言語リスト表示・チェックまで

--- js/script.coffee
+++ js/script.coffee
@@ -1,3 +1,13 @@
+Array.prototype.shuffle = ->
+  i = this.length
+  while(i)
+    j = Math.floor(Math.random()*i)
+    t = this[--i]
+    this[i] = this[j]
+    this[j] = t
+  return this
+
+
 $ ->
   preview_wrap = $('#preview_wrap')
   previewReload(preview)
@@ -15,13 +25,29 @@ $ ->
 previewReload = (preview)->
   # 必要な値を修得
   color = {}
-  size = $('.size-select :checked')
   color.bg = $('#color-bg').val()
   color.text = $('#color-text').val()
   color.check = $('#color-check').val()
+
+  size = $('.size-select :checked')
   width = size.data('width')
   height = size.data('height')
-  console.log width, height
+
+  langs = []
+  $('.langs input').each ->
+    lang =  $(this).val()
+    checked = if $(this).is(':checked') then "<i class='fa fa-check fa-stack-1x' />" else ''
+    str = "<li>
+    <span class='fa-stack'>
+      <i class='fa fa-square-o fa-stack-1x' />
+      #{checked}
+    </span>
+    #{lang}
+    </li>"
+    langs.push str
+    langs.push str
+  #langs.shuffle()
+
 
   # 反映
   preview = $('#preview')
@@ -31,5 +57,7 @@ previewReload = (preview)->
     width: width / 4,
     height: height / 4
   })
+  preview.find('ul').html(langs.join(' '))
+  preview.find('.fa-check').css('color', '#'+color.check)
 
   #チェックボックスの色変える
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 (function() {
   var previewReload;
 
+  Array.prototype.shuffle = function() {
+    var i, j, t;
+    i = this.length;
+    while (i) {
+      j = Math.floor(Math.random() * i);
+      t = this[--i];
+      this[i] = this[j];
+      this[j] = t;
+    }
+    return this;
+  };
+
   $(function() {
     var preview_offset, preview_wrap;
     preview_wrap = $('#preview_wrap');
@@ -19,22 +31,32 @@
   });
 
   previewReload = function(preview) {
-    var color, height, size, width;
+    var color, height, langs, size, width;
     color = {};
-    size = $('.size-select :checked');
     color.bg = $('#color-bg').val();
     color.text = $('#color-text').val();
     color.check = $('#color-check').val();
+    size = $('.size-select :checked');
     width = size.data('width');
     height = size.data('height');
-    console.log(width, height);
+    langs = [];
+    $('.langs input').each(function() {
+      var checked, lang, str;
+      lang = $(this).val();
+      checked = $(this).is(':checked') ? "<i class='fa fa-check fa-stack-1x' />" : '';
+      str = "<li> <span class='fa-stack'> <i class='fa fa-square-o fa-stack-1x' /> " + checked + " </span> " + lang + " </li>";
+      langs.push(str);
+      return langs.push(str);
+    });
     preview = $('#preview');
-    return preview.css({
+    preview.css({
       backgroundColor: '#' + color.bg,
       color: '#' + color.text,
       width: width / 4,
       height: height / 4
     });
+    preview.find('ul').html(langs.join(' '));
+    return preview.find('.fa-check').css('color', '#' + color.check);
   };
 
 }).call(this);
